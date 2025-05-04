ae84c52bdd79df0920276857a2898dadc1bc3167
mostafa <mostafarasekh5@gmail.com>
2025-05-04 18:47:35 +0300

    update dio concumer

--- lib/core/api/dio_consumer.dart
+++ lib/core/api/dio_consumer.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
 import 'package:new_structure/config/routes/page_name.dart';
@@ -8,6 +9,7 @@ import 'package:new_structure/core/api/end_points.dart';
 import 'package:new_structure/core/errors/api/api_response_codes.dart';
 import 'package:new_structure/core/helpers/cache_helper.dart';
 import 'package:new_structure/core/utils/functions/kprint.dart';
+import 'package:new_structure/core/utils/keys_manager.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 /// This class is responsible for handling api calls using Dio package
@@ -15,9 +17,10 @@ class DioConsumer implements ApiConsumer {
   final Dio dio;
 
   DioConsumer({required this.dio}) {
+    // for accepting self-signed certificates => http not secure like https
     dio.httpClientAdapter = IOHttpClientAdapter(
-      // ignore: deprecated_member_use
-      onHttpClientCreate: (client) {
+      createHttpClient: () {
+        final client = HttpClient();
         client.badCertificateCallback =
             (X509Certificate cert, String host, int port) => true;
         return client;
@@ -43,17 +46,12 @@ class DioConsumer implements ApiConsumer {
         PrettyDioLogger(
           requestHeader: true,
           requestBody: true,
-          responseBody: true,
-          responseHeader: false,
-          error: true,
-          compact: true,
-          maxWidth: 90,
         ),
 
         InterceptorsWrapper(
           onRequest: (options, handler) async {
             // For token handling
-            final token = CacheHelper.getStringData('token');
+            final token = CacheHelper.getStringData(KeysManager.token);
 
             if (token != null) {
               // Set the Authorization header with the cached access token
@@ -66,14 +64,14 @@ class DioConsumer implements ApiConsumer {
           //   return handler.next(response);
           // },
           onError: (error, handler) {
-            kprint("Error from DioConsumer: ${error.toString()}");
+            kprint("Error from DioConsumer: ${error.message}");
 
             if (error.response?.statusCode == APIResponseCodes.unauthorized ||
                 error.response?.statusCode == APIResponseCodes.forbidden) {
-              // User is unauthorized or forbidden
+              /// User is unauthorized or forbidden
 
               // Clear cashed data
-
+              CacheHelper.removeData(key: KeysManager.token);
               // Navigate to login page
               RouteManager.navigatorKey.currentState!.pushNamedAndRemoveUntil(
                 PageName.login,
@@ -91,10 +89,13 @@ class DioConsumer implements ApiConsumer {
   @override
   Future get({
     required String path,
+    Map<String, dynamic>? body,
+    bool isFormData = false,
     Map<String, dynamic>? queryParameters,
   }) async {
     final response = await dio.get(
       path,
+      data: isFormData ? FormData.fromMap(body!) : body,
       queryParameters: queryParameters,
     );
     return response.data;
@@ -104,11 +105,13 @@ class DioConsumer implements ApiConsumer {
   Future patch({
     required String path,
     Map<String, dynamic>? body,
+    bool isFormData = false,
     Map<String, dynamic>? queryParameters,
   }) async {
     final response = await dio.patch(
       path,
-      data: body,
+      data: isFormData ? FormData.fromMap(body!) : body,
+      queryParameters: queryParameters,
     );
 
     return response.data;
@@ -124,6 +127,7 @@ class DioConsumer implements ApiConsumer {
     final response = await dio.post(
       path,
       data: isFormData ? FormData.fromMap(body!) : body,
+      queryParameters: queryParameters,
     );
 
     return response.data;
@@ -133,18 +137,27 @@ class DioConsumer implements ApiConsumer {
   Future put({
     required String path,
     Map<String, dynamic>? body,
+    bool isFormData = false,
     Map<String, dynamic>? queryParameters,
-  }) {
-    throw UnimplementedError();
+  }) async {
+    final response = await dio.put(
+      path,
+      data: isFormData ? FormData.fromMap(body!) : body,
+      queryParameters: queryParameters,
+    );
+    return response.data;
   }
 
   @override
   Future delete({
     required String path,
+    Map<String, dynamic>? body,
+    bool isFormData = false,
     Map<String, dynamic>? queryParameters,
   }) async {
     final resposne = await dio.delete(
       path,
+      data: isFormData ? FormData.fromMap(body!) : body,
       queryParameters: queryParameters,
     );
 
--- lib/core/utils/keys_manager.dart
+++ lib/core/utils/keys_manager.dart
@@ -1,4 +1,5 @@
 class KeysManager {
+  static const String token = 'token';
   static const String isDarkMode = 'isDarkMode';
   static const String locale = 'locale';
 }
