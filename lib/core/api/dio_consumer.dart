fb09b17f6c80083053eaf204656429a4a25baa40
mostafa <mostafarasekh5@gmail.com>
2025-05-04 19:07:11 +0300

    update dio concumer

--- lib/core/api/dio_consumer.dart
+++ lib/core/api/dio_consumer.dart
@@ -35,8 +35,6 @@ class DioConsumer implements ApiConsumer {
       connectTimeout: const Duration(seconds: 30),
       headers: {
         'Content-Type': 'application/json',
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
       },
     );
 
